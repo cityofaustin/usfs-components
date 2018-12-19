006fb7df6be7a2ca9904610a3170b69bd263c900
Brian Smith <briaguya@gmail.com>
2018-12-19 04:42:53 -0600

    try replacing the google maps

--- src/LocationPickerWidget.js
+++ src/LocationPickerWidget.js
@@ -1,11 +1,48 @@
-import React from "react";
-import LocationPicker from "react-location-picker";
+import React, { Component } from "react";
+import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
 
-/* Default position */
-const defaultPosition = {
-  lat: 27.9878,
-  lng: 86.925
-};
+const Map = ReactMapboxGl({
+  accessToken:
+    "pk.eyJ1IjoiY3Jvd2VhdHgiLCJhIjoiY2o1NDFvYmxkMHhkcDMycDF2a3pseDFpZiJ9.UcnizcFDleMpv5Vbv8Rngw"
+});
+
+class SelectLocationMap extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onStyleLoad = this.onStyleLoad.bind(this);
+  }
+
+  onStyleLoad({ map }) {
+    map.resize();
+  }
+
+  render() {
+    const { lat, lng, crossingMoved } = this.props;
+    return (
+      <Map
+        style={"mapbox://styles/croweatx/cjow5d6cd3l7g2snrvf17wf0r"}
+        center={[lng, lat]}
+        onStyleLoad={this.onStyleLoad}
+      >
+        <Layer
+          type="symbol"
+          id="selectedLocation"
+          layout={{
+            "icon-image": "marker-open-small",
+            "icon-allow-overlap": true
+          }}
+        >
+          <Feature
+            coordinates={[lng, lat]}
+            draggable={true}
+            onDragEnd={crossingMoved}
+          />
+        </Layer>
+      </Map>
+    );
+  }
+}
 
 export default class LocationPickerWidget extends React.Component {
   constructor(props) {
@@ -33,12 +70,7 @@ export default class LocationPickerWidget extends React.Component {
       <div>
         <h1>{this.state.address}</h1>
         <div>
-          <LocationPicker
-            containerElement={<div style={{ height: "100%" }} />}
-            mapElement={<div style={{ height: "400px" }} />}
-            defaultPosition={defaultPosition}
-            onChange={this.handleLocationChange}
-          />
+          <SelectLocationMap />
         </div>
       </div>
     );
