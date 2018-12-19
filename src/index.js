7435bb40f81894141d390dd669b6a6435d39c3f2
Brian Smith <briaguya@gmail.com>
2018-12-18 20:16:04 -0600

    get em all in there

--- src/index.js
+++ src/index.js
@@ -1,33 +1,19 @@
-import "flatpickr/dist/themes/material_green.css";
+import DateTimeWidget from "./DateTimeWidget";
+import DateTimeReviewWidget from "./DateTimeReviewWidget";
+import LocationPickerWidget from "./LocationPickerWidget";
+import LocationReviewWidget from "./LocationReviewWidget";
+import FileUploadWidget from "./FileUploadWidget";
+import FileUploadReviewWidget from "./FileUploadReviewWidget";
+import OfficerDetailsDisplayWidget from "./OfficerDetailsDisplayWidget";
+import WitnessDetailsDisplayWidget from "./WitnessDetailsDisplayWidget";
 
-import React from "react";
-import Flatpickr from "react-flatpickr";
-
-export class DateTimeWidget extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      dates: [props.value]
-    };
-
-    this.onChange = this.onChange.bind(this);
-  }
-
-  onChange({ dates, datestring }) {
-    this.props.onChange(datestring);
-    this.setState({
-      dates
-    });
-  }
-
-  render() {
-    return (
-      <Flatpickr
-        data-enable-time
-        value={this.state.dates}
-        onChange={(dates, datestring) => this.onChange({ dates, datestring })}
-      />
-    );
-  }
-}
+export {
+  DateTimeWidget,
+  DateTimeReviewWidget,
+  LocationPickerWidget,
+  LocationReviewWidget,
+  FileUploadWidget,
+  FileUploadReviewWidget,
+  OfficerDetailsDisplayWidget,
+  WitnessDetailsDisplayWidget
+};
