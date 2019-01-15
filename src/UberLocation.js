37cd1e9ec0a553ca7fc96a585ac237992e31c81d
Brian Smith <briaguya@gmail.com>
2019-01-15 10:21:24 -0600

    uber map loads with react 16

--- src/UberLocation.js
+++ src/UberLocation.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import ReactMapGL from 'react-map-gl';
+
+class Map extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewport: {
+        width: 400,
+        height: 400,
+        latitude: 37.7577,
+        longitude: -122.4376,
+        zoom: 8,
+      },
+    };
+  }
+
+  render() {
+    return (
+      <ReactMapGL
+        {...this.state.viewport}
+        onViewportChange={viewport => this.setState({ viewport })}
+        mapboxApiAccessToken={
+          'pk.eyJ1IjoiY3Jvd2VhdHgiLCJhIjoiY2o1NDFvYmxkMHhkcDMycDF2a3pseDFpZiJ9.UcnizcFDleMpv5Vbv8Rngw'
+        }
+      />
+    );
+  }
+}
 
 export default class UberLocation extends React.Component {
   render() {
-    return <div>BLARG</div>;
+    return <Map />;
   }
 }
