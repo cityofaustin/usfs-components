96cf6c2ed09e148f79f18f942cf8adc85a36c702
easherma <ericandrewsherman@gmail.com>
2019-02-19 12:09:46 -0600

    remove placeholder language

--- src/WitnessDetailsDisplayWidget.js
+++ src/WitnessDetailsDisplayWidget.js
@@ -2,5 +2,5 @@ import React from 'react';
 import classnames from 'classnames';
 
 export default function WitnessDetailsDisplayWidget(props) {
-  return <div>Placeholder Witness Details</div>;
+  return <div></div>;
 }
