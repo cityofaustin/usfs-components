c716eb66aa25616c6b46b0fef16007304f49d6e6
Niicck <niicck@users.noreply.github.com>
2019-03-13 20:40:50 -0500

    restrict flatpickr to past dates

--- src/DateTimeWidget.js
+++ src/DateTimeWidget.js
@@ -1,8 +1,11 @@
 import 'flatpickr/dist/themes/material_green.css';
+import './DateTimeWidget.css';
 
 import React from 'react';
+import Emoji from './Emoji';
 import Flatpickr from 'react-flatpickr';
 
+// Returns datestring in the form "2011-11-11 11:11"
 export default class DateTimeWidget extends React.Component {
   constructor(props) {
     super(props);
@@ -18,9 +21,17 @@ export default class DateTimeWidget extends React.Component {
     return (
       <Flatpickr
         data-enable-time
+        options={{
+          maxDate: 'today'
+        }}
         value={[this.props.value]}
         onChange={(dates, datestring) => this.onChange({ dates, datestring })}
-      />
+      >
+        <input className={`flatpickr-input-box`} type="text" data-input />
+        <span className="flatpickr-input-button" title="toggle" data-toggle>
+          <Emoji symbol="📅" label="calendar"/>
+        </span>
+      </Flatpickr>
     );
   }
 }
